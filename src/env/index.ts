2ecdd2cebe56d14a78b09a4f8074ac8dbe1941c4
Joao <joaovitortonetomachado2310@gmail.com>
2025-07-06 21:43:23 -0300

    estrutura basica

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+// process.env: { NODE_ENV }
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  // derruba a aplicacao
+  throw new Error('Invalid environment variables')
+}
+
+// vai trazer os dados das variaveis ambiente
+export const env = _env.data
